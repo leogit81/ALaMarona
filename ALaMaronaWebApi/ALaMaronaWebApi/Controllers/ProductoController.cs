a0091de61afd7dc2ea258237ec34c9e043f7446c
Oneto Leonardo <Oneto@kapsch.net>
2017-11-26 20:00:26 -0300

    se hicieron varios cambios a los proyectos incluidos y se modificaron script para popular base de datos en el proyecto web api

---
+++ ALaMaronaWebApi/ALaMaronaWebApi/Controllers/ProductoController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+using ALaMarona.Domain.DTOs;
+using ALaMarona.Domain.Entities;
+using ALaMarona.Core.Controller;
+
+namespace ALaMaronaWebApi.Controllers
+{
+    [RoutePrefix("api/Producto")]
+    public class ProductoController : GenericController<Producto, long, ProductoDTO, long>
+    {
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+select *
+from alamarona.dbo.pais
+select *
+from alamarona.dbo.Provincia
+
+select *
+from alamarona.dbo.Localidad
+
+select *
+from ALaMarona.dbo.Direccion
